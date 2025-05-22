4ae54d1c5cd1c3bb2c3a5e678f01b415859dddbf
dev1joe <yousef.m.dev1@gmail.com>
2025-05-22 17:07:01 +0300

    modifications on logging

--- realtime_detection.py
+++ realtime_detection.py
@@ -19,27 +19,11 @@ class IntrusionModel(nn.Module):
     def forward(self, x):
         return self.network(x)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model = IntrusionModel()
-model.load_state_dict(torch.load("intrusion_model_weights.pth", map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("./save/intrusion_model_weights.pth", map_location=device))
 model.eval()
-
-def extract_features(packet):
-    try:
-        length = len(packet)
-        proto = 1 if packet.haslayer("TCP") else 0
-        login_attempts = 3
-        session_duration = 500.0    
-        encryption = 0               
-        reputation_score = 0.5       
-        failed_logins = 1
-        browser = 2              
-        unusual_time = 0
-        dummy = 0            
-
-        return [length, proto, login_attempts, session_duration,
-                encryption, reputation_score, failed_logins, browser, unusual_time, dummy]
-    except:
-        return None
 
 def classify_packet(packet):
     features = extract_features(packet)
@@ -49,7 +33,9 @@ def classify_packet(packet):
         label = "Attack" if output.item() > 0.5 else "Normal"
         log_entry = f"[{datetime.now()}] {packet.summary()} => {label}\n"
         print(log_entry.strip())
-        with open("log.txt", "a") as log_file:
+
+        log_file = f'./logs/packet_log_{datetime.today()}.txt'
+        with open(log_file, "a") as log_file:
             log_file.write(log_entry)
 
 def extract_features(packet):
@@ -61,12 +47,17 @@ def extract_features(packet):
         encryption = 0
         reputation_score = 0.5
         failed_logins = 1
-        browser = 2
         unusual_time = 0
-
         source_bytes = 1000
         dest_bytes = 800
         num_root = 0
+        
+        # browser info
+        browser = 'unknown'
+        # if packet.haslayer('Raw'):
+        #     payload = packet['Raw'].load
+        #     if b"User-Agent" in payload:
+        #         browser = payload.split(b"User-Agent:")[1].split(b"\r\n")[0]
 
         return [length, proto, login_attempts, session_duration,
                 encryption, reputation_score, failed_logins,
@@ -74,6 +65,9 @@ def extract_features(packet):
     except:
         return None
 
-
 print("Sniffing started... Press Ctrl+C to stop.")
-sniff(prn=classify_packet, store=0)
+
+try: 
+    sniff(prn=classify_packet, store=0)
+except Exception as e:
+    print(f"Error: {e}")
